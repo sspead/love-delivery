3a7de4cb38c6d3f810c97d768222c57ea7d44ce7
Love <love@delivery.app>
2026-06-25 21:53:16 +0800

    Build to docs for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/love-delivery/',
+  build: { outDir: 'docs' },
   plugins: [
     react(),
     tailwindcss(),
